7cf75907eda96cabda5860fe19e7293764d729d3
Umar Alvi <117147982+umar-alvi-ops@users.noreply.github.com>
2024-10-29 20:05:33 +0500

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,6 @@
+// async function test(){
+//     const api= await (await fetch("https://jsonplaceholder.typicode.com/posts/")).json();
+//     console.log(api)
+//     document.write(JSON.stringify())
+// }
+// test();
